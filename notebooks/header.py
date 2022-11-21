9f4b42385d7e7ef0f12cd7627a27120d789903fc
qaztronic <qaztronic@gmail.com>
2022-11-21 13:51:50 -0800

    updates to notebooks

---
+++ notebooks/header.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy import signal
+from scipy import linalg
+import control
+import matplotlib.pyplot as plt
+
+import sys
+
+if '../sdfpy' not in sys.path:
+  sys.path.insert(0,'../sdfpy')
+
+import sd_sim
+import sdfpy as sdf
+
+OSR = 256      # oversample ratio
+fb = 22050     # nyquist
+fs = OSR*2*fb  # sampling frequency
+ts = 1/fs      # sampling period
